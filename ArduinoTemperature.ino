b9961459035580a27a3cd1a7db876f1dc5e050e7
Bruno DELATTRE <delattreb@gmail.com>
2018-11-11 21:18:31 +0100

    Fix

--- ArduinoTemperature.ino
+++ ArduinoTemperature.ino
@@ -9,15 +9,29 @@ libLCD lcd;
 libSI7021 si7021;
 SoftwareSerial esp8266(RX_PIN, TX_PIN);
 
-String datestr, timestr;
-static unsigned long previousMillis = 0;
+String datestr, timestr, wific, cmd;
+static unsigned long previousMillisAcq = 0, previousMillisLog = 0;
 unsigned long currentMillis;
 float sitemp, sihum, offsetTemp[MAXDEVICE] = { -3.5,-2.9,-3.0,-0.0,-0.0 }, offsetHum[MAXDEVICE] = { -18.0,-17.0,-13.0,-0.0,-0.0 }; // Offet for devices
+String inputString = "";
+bool stringComplete = false;
 #pragma endregion
 
 //
-//* Setup
-// param: 
+// serialEvent
+//
+void serialEvent() {
+	while (esp8266.available()) {
+		char inChar = (char)esp8266.read();
+		inputString += inChar;
+		if (inChar == '\n') {
+			stringComplete = true;
+		}
+	}
+}
+
+//
+// Setup
 //
 void setup() {
 	esp8266.begin(SERIAL_BAUD);
@@ -41,28 +55,38 @@ void setup() {
 //
 void loop() {
 	currentMillis = millis();
+	if (currentMillis - previousMillisAcq >= ACQ_FREQUENCY) {
+		sitemp = si7021.getTemperature() + offsetTemp[DEVICE_NUMBER - 1];
+		sihum = si7021.getHumidity() + offsetHum[DEVICE_NUMBER - 1];
+		lcd.displayData(sitemp, sihum);
+		previousMillisAcq = currentMillis;
+	}
+	if (stringComplete) {
+		wific = inputString;
+		inputString = "";
+		stringComplete = false;
 
-	sitemp = si7021.getTemperature() + offsetTemp[DEVICE_NUMBER - 1];
-	sihum = si7021.getHumidity() + offsetHum[DEVICE_NUMBER - 1];
-	lcd.displayData(sitemp, sihum);
-
-	// Check WiFi connexion
-	if (esp8266.available()) {
-		String wific = esp8266.readString();
-		if (wific.startsWith("WK", 0))
-			lcd.displayWiFiKo();
-		if (wific.startsWith("WO", 0))
-			lcd.displayWiFiOk();
-		if (wific.startsWith("MK", 0))
-			lcd.displayMQTTKo();
-		if (wific.startsWith("MO", 0))
-			lcd.displayMQTTOk();
-		if (wific.startsWith("S", 0))
-			lcd.displayWiFiDbSignal(wific.substring(1, 4));
+		cmd = "";
+		for (int i = 0; i < wific.length(); i++) {
+			cmd += (char)wific[i];
+			if ((char)wific[i] == '\n') {
+				if (cmd.startsWith("WK"))
+					lcd.displayWiFiKo();
+				if (cmd.startsWith("WO"))
+					lcd.displayWiFiOk();
+				if (cmd.startsWith("MK"))
+					lcd.displayMQTTKo();
+				if (cmd.startsWith("MO"))
+					lcd.displayMQTTOk();
+				if (cmd.startsWith("S"))
+					lcd.displayWiFiDbSignal(wific.substring(1, 4));
+				cmd = "";
+			}
+		}
 	}
 
 	// Acquisition
-	if (currentMillis - previousMillis >= LOG_FREQUENCY) {
+	if (currentMillis - previousMillisLog >= LOG_FREQUENCY) {
 		String dataT = "T" + String(DEVICE_NUMBER) + ':' + String(sitemp);
 		String dataH = "H" + String(DEVICE_NUMBER) + ':' + String(sihum);
 #ifdef DEBUG
@@ -72,7 +96,7 @@ void loop() {
 		esp8266.println(dataT);
 		delay(SERIAL_DELAY);
 		esp8266.println(dataH);
-		previousMillis = currentMillis;
+		previousMillisLog = currentMillis;
 	}
-	delay(ACQ_FREQUENCY);
+	serialEvent();
 }
